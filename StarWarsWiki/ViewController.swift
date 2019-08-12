770c2595e0c6ccda305977775bfc39c70dac8cb0
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-08-12 14:24:33 -0400

    first commit

---
+++ StarWarsWiki/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWarsWiki
+//
+//  Created by Ibraheem rawlinson on 8/12/19.
+//  Copyright © 2019 Ibraheem rawlinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
