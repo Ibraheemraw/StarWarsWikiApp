bcb8195f632e4bf6d461515cbcbaf5ffcbd2f37d
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-08-12 15:09:07 -0400

    added the third model for the planet model

--- StarWarsWiki/Models/PlanetListModel.swift
+++ StarWarsWiki/Models/PlanetListModel.swift
@@ -1,9 +1,24 @@
-//
-//  PlanetListModel.swift
-//  StarWarsWiki
-//
-//  Created by Ibraheem rawlinson on 8/12/19.
-//  Copyright © 2019 Ibraheem rawlinson. All rights reserved.
-//
-
 import Foundation
+
+//MARK: - Planet List Model
+struct StarWarsPlanetList: Codable {
+    let count: Int
+    let previous: String?
+    let next: String?
+    let planets: [Planet]
+    enum CodingKeys: String, CodingKey {
+        case count
+        case previous
+        case next
+        case planets = "results"
+    }
+}
+
+struct Planet: Codable {
+    let name: String
+    let diameter: String
+    let climate: String
+    let gravity: String
+    let population: String
+    let created: String
+}
